bc780fab1e2fd72155922cae5d4b9e2364a5ac91
Robe Santoro z800 <robe.santoro@live.com>
2022-04-28 00:15:04 +0200

    Added Plane, Box, Spring(notUsed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-
 import { Suspense } from "react";
 
-import { Html, MeshDistortMaterial, RoundedBox, Sphere } from '@react-three/drei';
 import { Canvas } from "@react-three/fiber";
+
+import { Html, MeshDistortMaterial, MeshReflectorMaterial, MeshWobbleMaterial, Plane, RoundedBox, Sphere } from '@react-three/drei';
 import { OrbitControls } from "@react-three/drei";
 
 import Helpers from "./components/helpers/helpers";
@@ -15,14 +15,14 @@ export default function App() {
   const MIN_DISTORT = 0.5
   const MAX_DISTORT = 1
 
-  const [distort, setDistort] = useState(MIN_DISTORT) ;
+  const [distort, setDistort] = useState(MIN_DISTORT);
 
   const incrementDistort = () => {
-    (distort==MIN_DISTORT) ? setDistort(MAX_DISTORT) : setDistort(MIN_DISTORT);
+    distort == MIN_DISTORT ? setDistort(MAX_DISTORT) : setDistort(MIN_DISTORT);
   }
 
   useEffect(() => {
-    console.log(distort)    
+    console.log(distort)
   }, [distort])
 
 
@@ -33,18 +33,31 @@ export default function App() {
         <Helpers />
         <OrbitControls />
 
-        <Sphere args={[1, 50, 100]} scale={1} onPointerDown={incrementDistort}>
-          <MeshDistortMaterial 
+        <Sphere args={[1, 50, 100]} scale={1} onClick={incrementDistort}>
+          <MeshDistortMaterial
             wireframe={false}
             color="#8352FD"
             attach="material"
             distort={distort}
-            speed={1.5}
+            speed={4}
             roughness={0} />
         </Sphere>
 
+        <RoundedBox args={[1, 1, 1]} position={[2, 0, 0]}>
+          <MeshWobbleMaterial
+            color="#f752fd"
+            factor={1}
+            speed={1} />
+        </RoundedBox>
+
+        <Plane args={[10, 10]}
+          position={[0, -2, 0]}
+          rotation={[-1.57, 0, 0]}>
+          <meshStandardMaterial color={"grey"} />
+        </Plane>
+
         <ambientLight args={[0xffffff]} intensity={0.1} />
-        <directionalLight position={[1, 2, 5]} intensity={1} />        
+        <directionalLight position={[1, 2, 5]} intensity={1} />
 
       </Suspense>
     </Canvas>
--- src/components/helpers/helpers.tsx
+++ src/components/helpers/helpers.tsx
@@ -5,8 +5,8 @@ import { Canvas } from "@react-three/fiber";
 export default function Helpers() {
   return (
     <>
-      <gridHelper args={[10, 10]} />
-      <axesHelper args={[2]} />
+      <gridHelper args={[10, 10]} position={[0, -2, 0]} />
+      {/* <axesHelper args={[2]} /> */}
       <Stats/>
     </>
   );
