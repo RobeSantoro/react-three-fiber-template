6fbc688ecd5f4b26fdbac12f30fd807ebfb684ef
Robe Santoro z800 <robe.santoro@live.com>
2022-04-27 22:57:03 +0200

    Added Distort State Interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { useState, useEffect } from "react";
+
+import { Suspense } from "react";
 
 import { Html, MeshDistortMaterial, RoundedBox, Sphere } from '@react-three/drei';
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
-
 import { OrbitControls } from "@react-three/drei";
 
 import Helpers from "./components/helpers/helpers";
@@ -10,6 +11,21 @@ import Helpers from "./components/helpers/helpers";
 const Loading = <Html><div>LOADING...</div></Html>;
 
 export default function App() {
+
+  const MIN_DISTORT = 0.5
+  const MAX_DISTORT = 1
+
+  const [distort, setDistort] = useState(MIN_DISTORT) ;
+
+  const incrementDistort = () => {
+    (distort==MIN_DISTORT) ? setDistort(MAX_DISTORT) : setDistort(MIN_DISTORT);
+  }
+
+  useEffect(() => {
+    console.log(distort)    
+  }, [distort])
+
+
   return (
     <Canvas>
       <Suspense fallback={Loading}>
@@ -17,12 +33,12 @@ export default function App() {
         <Helpers />
         <OrbitControls />
 
-        <Sphere args={[1, 50, 100]} scale={1}>
+        <Sphere args={[1, 50, 100]} scale={1} onPointerDown={incrementDistort}>
           <MeshDistortMaterial 
             wireframe={false}
             color="#8352FD"
             attach="material"
-            distort={0.5}
+            distort={distort}
             speed={1.5}
             roughness={0} />
         </Sphere>
